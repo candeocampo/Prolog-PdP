57b18894c37b246089ec6c032f03ffd8c91fe476
candeocampo <cocampo@frba.utn.edu.ar>
2024-07-22 15:52:06 -0300

    fin parcial carcel

---
+++ festivalRock.pl
@@ -0,0 +1,40 @@
+%% BASE DE CONOMIENTOS
+
+% festival(NombreDelFestival, Bandas, Lugar).
+% Relaciona el nombre de un festival con la lista de los nombres de bandas que tocan en él y el lugar dónde se realiza.
+festival(lollapalooza, [gunsAndRoses, theStrokes, ..., littoNebbia], hipódromoSanIsidro).
+
+% lugar(nombre, capacidad, precioBase).
+% Relaciona un lugar con su capacidad y el precio base que se cobran las entradas ahí.
+lugar(hipódromoSanIsidro, 85000, 3000).
+
+% banda(nombre, nacionalidad, popularidad).
+% Relaciona una banda con su nacionalidad y su popularidad.
+banda(gunsAndRoses, eeuu, 69420).
+
+% entradaVendida(NombreDelFestival, TipoDeEntrada).
+% Indica la venta de una entrada de cierto tipo para el festival 
+% indicado.
+% Los tipos de entrada pueden ser alguno de los siguientes: 
+%     - campo
+%     - plateaNumerada(Fila)
+%     - plateaGeneral(Zona).
+entradaVendida(lollapalooza, campo).
+entradaVendida(lollapalooza, plateaNumerada(1)).
+entradaVendida(lollapalooza, plateaGeneral(zona2)).
+
+% plusZona(Lugar, Zona, Recargo)
+% Relacion una zona de un lugar con el recargo que le aplica al precio de las plateas generales.
+plusZona(hipódromoSanIsidro, zona1, 1500).
+
+
+
+
+
+
+
+
+
+
+
+

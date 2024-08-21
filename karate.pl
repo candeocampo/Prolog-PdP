3ee47dc051514f24318da4dae9cb896229aded1b
candeocampo <cocampo@frba.utn.edu.ar>
2024-08-20 22:58:00 -0300

    pepe

--- karate.pl
+++ karate.pl
@@ -83,20 +83,48 @@ soloSabePatear(Alumno):-
     forall(member(Habilidad,Habilidades),patada(Habilidad)).
 
 % Punto 7
+potencialesSemifinalistas(Alumno):-
+    esAptoParaTorneo(Alumno),
+    tieneMaestro(Alumno),
+    habilidadConBuenEstilo(Alumno).
 
+tieneMaestro(Alumno):-
+    alumnoDe(Maestro,Alumno),
+    alumnoDe(Maestro,OtroAlumno),
+    Alumno \= OtroAlumno.
 
+punteriaHabilidad(patadaDeGiro(_,Punteria,_),Punteria).
+punteriaHabilidad(patadaVoladora(_,_,_,Punteria),Punteria).
 
+habilidadConBuenEstilo(Alumno):-
+    destreza(Alumno,_,Habilidades),
+    obtenerPunteria(Habilidades,90).
 
+habilidadConBuenEstilo(Alumno):-
+    destreza(Alumno,_,Habilidades),
+    obtenerPotencia(Habilidades,100).
 
+obtenerPunteria(Habilidades,Punteria):-
+    member(HabilidadPunteria,Habilidades),
+    punteriaHabilidad(HabilidadPunteria,Punteria).
 
+obtenerPotencia(Habilidades,Potencia):-
+    member(HabilidadPotencia,Habilidades),
+    potenciaHabilidad(HabilidadPotencia,Potencia).
 
+% Punto 8
+semifinalistas(ListaAlumnos):-
+    findall(Alumno, potencialesSemifinalistas(Alumno),ListaAlumnos).
 
+% Punto 9
+%% El polimorfismo en Prolog se ve en la forma en que diferentes predicados trabajan con distintos 
+%% tipos de datos o estructuras de forma genérica. Por ejemplo, potenciaHabilidad/2 es un predicado 
+%% que funciona con varias formas de habilidades (patadaRecta, patadaDeGiro, etc.) 
+%% sin necesidad de modificar el código para cada tipo específico. Esto permite que 
+%% potenciaHabilidad/2 se aplique a cualquier habilidad de manera uniforme, sin importar su tipo específico.
 
-
-
-
-
-
+% El uso de predicados de orden superior, como member/2, forall/2, y findall/3, 
+% permite que se manipulen listas y se realicen comprobaciones o búsquedas de manera abstracta y reutilizable.
 
 
 

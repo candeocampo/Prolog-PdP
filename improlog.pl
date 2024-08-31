abf7ef7930412dccb294e6c4079684dd64733cee
candeocampo <cocampo@frba.utn.edu.ar>
2024-08-31 01:24:32 -0300

    Punto 2, punto 3 y punto 4 de Improlog

--- improlog.pl
+++ improlog.pl
@@ -44,8 +44,51 @@ tocaInstrumentoEnBanda(Grupo,Persona,Rol):-
     instrumento(Instrumento,Rol).
 
 % Punto 2
-destaca(Persona,Grupo):-
-    
+seDestaca(Persona,Grupo):-
+    nivelConQueToca(Grupo,Persona,Nivel),
+    forall((nivelConQueToca(Grupo,OtraPersona,OtroNivel),Persona\=OtraPersona),Nivel > OtroNivel +2).
+
+% Buscamos abstraer esto en otra forma:
+% integrante(Grupo,Persona,Instrumento),
+% nivelQueTiene(Persona,Instrumento,Nivel)
+
+nivelConQueToca(Grupo,Persona,Nivel):-
+    integrante(Grupo,Persona,Instrumento),
+    nivelQueTiene(Persona,Instrumento,Nivel).
+
+% Punto 3
+% grupo(Grupo,Tipo)
+grupo(vientosDelEste,bigBand).
+grupo(sophieTrio,formacion([contrabajo,guitarra,violin])).
+grupo(jazzmin,formacion([bateria,bajo,trompeta,piano,guitarra])).
+
+% Punto 4
+esDeViento(Instrumento):-
+    instrumento(Instrumento,melodico(viento)).
+
+hayCupo(Instrumento,Grupo):-
+    grupo(Grupo,bigBand),
+    esDeViento(Instrumento).
+hayCupo(Instrumento,Grupo):-
+    instrumento(Instrumento,_), % DEBES PONERLO PARA LIGAR VARIABLES!
+    grupo(Grupo,Tipo),
+    instrumentoQueSirve(Tipo,Instrumento),
+    not(integrante(Grupo,_,Instrumento)). % NO hay alguien que ya lo toque en el grupo.
+
+% este no es inversible ¡debes ligar la variable antes de utilizarlo!
+instrumentoQueSirve(formacion(Instrumentos),Instrumento):-
+    member(Instrumento,Instrumentos).
+instrumentoQueSirve(bigBand,Instrumento):-
+    esDeViento(Instrumento).
+instrumentoQueSirve(bigBand,bateria).
+instrumentoQueSirve(bigBand,bajo).
+instrumentoQueSirve(bigBand,piano).
+
+
+% Punto 5
+
+
+
 
 
 

f837ddc9fbb752791b2665731c39ad68f46c7803
candeocampo <cocampo@frba.utn.edu.ar>
2024-09-02 17:12:00 -0300

    subo pulp Fiction fin

--- pulpFiction.pl
+++ pulpFiction.pl
@@ -85,30 +85,84 @@ estaEnProblemas(Personaje):-
     personaje(Persona,boxeador).
 
 % Punto 4
-sanCayetano(Personaje).
+sanCayetano(Personaje):-
+    personaje(Personaje,_),
+    forall(tieneCerca(Personaje,Empleado),encargo(Personaje,Empleado,_)).
 
+tieneCerca(Personaje,OtroPersonaje):-
+    amigo(Personaje,OtroPersonaje).
+tieneCerca(Personaje,OtroPersonaje):-
+    trabajaPara(Personaje,OtroPersonaje).
 
+% Punto 5
+tieneMasEncargos(Personaje,OtroPersonaje):-
+    cantidadEncargos(Personaje,Cantidad),
+    cantidadEncargos(OtroPersonaje,Cantidad2),
+    Cantidad > Cantidad2.
 
+cantidadEncargos(Personaje,Cantidad):-
+    encargo(_,Personaje,_),
+    findall(Encargo,encargo(_,Personaje,Encargo),Encargos),
+    length(Encargos,Cantidad).
 
+masAtareado(Personaje):-
+    personaje(Personaje,_),
+    forall((personaje(OtroPersonaje,_),Personaje \= OtroPersonaje),
+    tieneMasEncargos(Personaje,OtroPersonaje)).
 
+% Punto 6
+personajesRespetables(Personaje):-
+    findall(PersonajeRespetable,esRespetable(PersonajeRespetable),Personaje).
 
+esRespetable(Personaje):-
+    personaje(Personaje,Actividad),
+    nivelRespeto(Actividad,Nivel),
+    Nivel > 9.
 
+nivelRespeto(actriz(Peliculas),Nivel):-
+    length(Peliculas,CantidadPeliculas),
+    Nivel is CantidadPeliculas // 10.
+nivelRespeto(mafioso(resuelveProblemas),10).
+nivelRespeto(mafioso(maton),1).
+nivelRespeto(mafioso(capo),20).
+nivelRespeto(_,0).
 
+% Punto 7
+% interactua con el segundo.
+interactua(cuidar(Persona),Persona).
+interactua(buscar(Persona,_),Persona).
+interactua(ayudar(Persona),Persona).
+% en caso de que tenga que interactuar con un amigo del segundo.
+interactua(cuidar(Persona),Amigo):-
+    amistad(Persona,Amigo).
+interactua(buscar(Persona),Amigo):-
+    amistad(Persona,Amigo).
+interactua(ayudar(Persona),Amigo):-
+    amistad(Persona,Amigo).
 
+hartoDe(Personaje,OtroPersonaje):-
+    encargo(_,Personaje,_), % lo que va acá tiene que ser el mismo predicado que uso en el forall para ligarlo.
+    personaje(OtroPersonaje,_),
+    forall(encargo(_,Personaje,Actividad),interactua(Actividad,OtroPersonaje)).
 
+% Punto 8
+% caracteristicas(Personaje,Caracteristicas).
+caracteristicas(vincent, [negro, muchoPelo, tieneCabeza]).
+caracteristicas(jules, [tieneCabeza, muchoPelo]).
+caracteristicas(marvin, [negro]).
 
+duoDiferenciable(Personaje,OtroPersonaje):-
+    relacion(Personaje,OtroPersonaje),
+    caracteristicas(Personaje,Caracteristica1),
+    caracteristicas(OtroPersonaje,Caracteristica2),
+    tieneCaracteristica(Caracteristica1,Caracteristica2).
 
-
-
-
-
-
-
-
-
-
-
-
+tieneCaracteristica(Caracteristica1,Caracteristica2):-
+    member(Caracteristica,Caracteristica1),
+    not(member(Caracteristica,Caracteristica2)).
+tieneCaracteristica(Caracteristica1,Caracteristica2):-
+    member(Caracteristica,Caracteristica2),
+    not(member(Caracteristica,Caracteristica1)).
 
 
 
